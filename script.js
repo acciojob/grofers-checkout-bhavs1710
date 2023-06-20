22ff3a3e6dc7f2c36ad5ebd5f75b391e59b09598
Bhawna Sharma <119921762+bhavs1710@users.noreply.github.com>
2023-06-20 22:34:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const priceElements = document.querySelectorAll('[data-ns-test="price"]');
+
+// Initialize a variable to store the total price
+let totalPrice = 0;
+
+// Loop through each price element and add its value to the total price
+priceElements.forEach((element) => {
+  totalPrice += Number(element.textContent);
+});
+
+// Set the total price in the table
+const totalElement = document.querySelector('[data-ns-test="grandTotal"]');
+totalElement.textContent = totalPrice;
+
+
   
 };
 
